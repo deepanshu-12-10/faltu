0712d8ee80c50565a45a1bb514c043a53be2f6cb
deepanshu-12-10 <deepanshu12.net@gmail.com>
2024-11-06 12:11:39 +0530

    Update faltu.cpp

--- faltu.cpp
+++ faltu.cpp
@@ -2,12 +2,15 @@
 using namespace std;
 int main(){
     int a,b;
+    int c;
+    cout<<"enter the c"<<endl;
+    cin>>c;
     cout<<"enter thee numbers"<<endl;
     cin>>a;
     cout<<"enter the second"<<endl;
     cin>>b;
     int sum=0;
-    sum=a+b;
+    sum=a+b+c;
     cout<<"sum"<<sum;
     return 0;
 
