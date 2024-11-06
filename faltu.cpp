a33e6bbf038edcf603dd633d1fbf59de76d92c8b
deepanshu-12-10 <deepanshu12.net@gmail.com>
2024-11-06 12:07:09 +0530

    sum

---
+++ faltu.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"enter thee numbers"<<endl;
+    cin>>a;
+    cout<<"enter the second"<<endl;
+    cin>>b;
+    int sum=0;
+    sum=a+b;
+    cout<<"sum"<<sum;
+    return 0;
+
+}
